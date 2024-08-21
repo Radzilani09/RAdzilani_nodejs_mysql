e03071832d0a1b992af3deb335fbacbe1740b06c
Radzilani <Radzilanimpho@unitacsolution.co.za>
2024-08-21 14:57:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express'),
+app = express(),
+bodyParser = require('body-parser');
+require('express-async-errors')
+
+
+const db = require('./db')
+employeeRoutes =require('./contollers/employee.controller')
+
+
+//middleware
+app.use(bodyParser.json())
+app.use('/api/employees',employeeRoutes)
+app.use((err,req,res,next) => {
+    console.log(err)
+    res.status(err.status||500).send('something went wrong !')
+})
+
+db.query("SELECT 1")
+.then(() => {
+    console.log('db connection succeeded')
+    app.listen(3300,
+        () => console.log('server started at 3300'))
+    
+})
+.catch(err => console.log('db connection failed \n'+err))
